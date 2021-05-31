5d1ef3db5ea7f2da370d98aa7860e688a3f06055
jayliam <51280073+jayliam@users.noreply.github.com>
2021-05-31 23:16:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,32 @@ var x = canvas.width/2;
 var y = canvas.height-80;
 var raquetteHeight = 10;
 var raquetteWidth = 75;
+
+var blockX = 20;
+var blockY = 20;
+var blockHeight = 15;
+var blockWidth = 38;
+
 var velocityX = 2;
 var velocityY = -2;
-var raquetteX = (canvas.width-raquetteWidth)/2;
+var raquetteX = (canvas.width - raquetteWidth)/2;
 var ball = 10;
 var rightPressed = false;
 var leftPressed = false;
+
+var blocks = new Array(3);
+
+var colonnes = 8;
+var lignes = 4;
+
+function initBlocks() {
+    for (var i = 0; i < colonnes; i++) {
+        blocks[i] = new Array(lignes);
+        for (var j = 0; j < lignes; j++) {
+            blocks[i][j] = 1;
+        }
+    }
+}
 
 document.addEventListener("keydown", KeyDown, false);
 document.addEventListener("keyup", KeyUp, false);
@@ -49,46 +69,70 @@ function drawraquette() {
     ctx.closePath();
 }
 
+function drawblocks() {
+    
+    ctx.beginPath();
+
+    for (var i in blocks) {
+        for (var j in blocks[i]) {
+            if (blocks[i][j] == 1) {
+
+                let bx = blockX + i*blockWidth + i*blockX;
+                let by = blockY + j*blockHeight +j*blockY;
+                ctx.rect( bx, by, blockWidth, blockHeight);
+            }
+        }
+    }
+
+    ctx.fillStyle = "#6095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function Collision() {
+    for (var i in blocks) {
+        for (var j in blocks[i]) {
+            if (blocks[i][j] == 1) {
+
+                let bx = blockX + i*blockWidth + i*blockX;
+                let by = blockY + j*blockHeight +j*blockY;
+
+                if (x >= bx && x <= bx + blockWidth && y >= by && y <= by + blockHeight ) {
+                    blocks[i][j] == 0;
+                    velocityY = -velocityY;
+
+                }
+
+            }
+        }
+    }
+
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawraquette();
+     
+    drawBall(); drawraquette();
+    Collision(); drawblocks();
     
-    if(x + velocityX > canvas.width-ball || x + velocityX < ball) {
+    
+    if(x  > canvas.width - ball || x  < ball) {
         velocityX = -velocityX;
     }
 
-    // if(y + velocityY > canvas.height-ball  || y + velocityY < ball ) {
-    //     velocityY = -velocityY;
-    //     }else { 
-    //             if (y + velocityY > canvas.height-ball) {
-    //                 if( x  > raquetteX && x < raquetteX + raquetteWidth  ) {
-    //                     velocityY = -velocityY;
-    //                 }else{
-    //                       alert("GAME OVER");
-    //     }
-        
-    // }
-
-    // }
-    if((y + velocityY > canvas.height-ball  - raquetteHeight  ) && (x  > raquetteX && x < raquetteX + raquetteWidth)   ) {
+    if((y  > canvas.height - ball - raquetteHeight  ) && (x  > raquetteX+2 && x < raquetteX + raquetteWidth-2)  ) {
         velocityY = -velocityY;
     }  
 
-    if(y + velocityY > canvas.height-ball     ) {
-        alert("gameover");
+    if(y > canvas.height - ball) {
+        if(alert('GameOver')){}
+        else    window.location.reload(); 
     }
 
-    if( y + velocityY < ball  ) {
+    if( y < ball  ) {
         velocityY = -velocityY;   
     }
     
-
-
-    // if(y + velocityY > canvas.height-ball - raquetteHeight || y + velocityY < ball ) {
-    //     velocityY = -velocityY;
-    // }
-
     if(rightPressed) {
         raquetteX += 7;
         if (raquetteX + raquetteWidth > canvas.width){
@@ -101,9 +145,8 @@ function draw() {
             raquetteX = 0;
         }
     }
+    x= x + velocityX;  y= y + velocityY;
     
-    x += velocityX;
-    y += velocityY;
 }
-
+initBlocks();
 setInterval(draw, 10);
